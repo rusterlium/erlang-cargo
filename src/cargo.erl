b4159576565554e689eb1ac25ae2e634252d514b
Benedikt Reinartz <filmor@gmail.com>
2020-05-03 15:04:29 +0200

    Revert forced Cargo.toml, not required

--- src/cargo.erl
+++ src/cargo.erl
@@ -24,13 +24,7 @@ init(Path) ->
 
 -spec init(file:name_all(), #{ atom() => _ }) -> cargo_opts:t() | no_return().
 init(Path, Opts) ->
-    CargoTomlPath = filename:join([Path, "Cargo.toml"]),
-    case filelib:is_dir(Path) andalso filelib:is_file(CargoTomlPath) of
-        true ->
-            cargo_opts:new(Opts#{ path => Path });
-        false ->
-            error(cargo_toml_not_found)
-    end.
+    cargo_opts:new(Opts#{ path => Path }).
 
 
 -spec metadata(cargo_opts:t()) -> #{ binary() => _ } | no_return().
