b20037bfbbd4bd3a6ef830a4086809c79fd4240b
Benedikt Reinartz <filmor@gmail.com>
2020-05-21 13:19:45 +0200

    Fix license and prepare for first release

--- mix.exs
+++ mix.exs
@@ -2,15 +2,16 @@ defmodule Cargo.MixProject do
   use Mix.Project
 
   def project do
-    # {:ok, rebar_config} = :file.consult("rebar.config")
+    {:ok, rebar_config} = :file.consult("rebar.config")
 
     {:ok, [{_, _, app_src}]} = :file.consult("src/cargo.app.src")
     version = :erlang.list_to_binary(Keyword.get(app_src, :vsn))
+    deps = Keyword.get(rebar_config, :deps)
 
     [
       app: :cargo,
       version: version,
-      deps: [{:jsx, "~> 2.8"}]
+      deps: deps
     ]
   end
 end
