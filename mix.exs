9a2b12ebc5ac4395670cebbfd3defba6fbb69c71
Benedikt Reinartz <filmor@gmail.com>
2020-05-25 10:58:43 +0200

    Work around bug in rebar3_hex to publish proper dep specs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule Cargo.MixProject do
 
     {:ok, [{_, _, app_src}]} = :file.consult("src/cargo.app.src")
     version = :erlang.list_to_binary(Keyword.get(app_src, :vsn))
-    deps = Keyword.get(rebar_config, :deps)
+    deps = Keyword.get(rebar_config, :deps) |>
+      Enum.map(
+        fn ({n, v}) -> {n, List.to_string(v)} end
+      )
 
     [
       app: :cargo,
